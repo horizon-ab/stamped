ea24b30f777f5062b4bd3f84441dcb30cfe878b9
Don Tran <75715248+dontran15@users.noreply.github.com>
2025-04-26 01:37:59 -0700

    misc

--- backend/src/apis/google-drive.ts
+++ backend/src/apis/google-drive.ts
@@ -1,28 +1,26 @@
 const { google } = require('googleapis');
 const path = require('path');
 const { Readable } = require('stream');
-const mime = require('mime-types'); // Import mime-types library
+const mime = require('mime-types');
 
-// 1. Authenticate
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, 'stamped-458002-22af2e382a01.json'), // your service account JSON
+  keyFile: path.join(__dirname, 'stamped-458002-22af2e382a01.json'), // service account file, note in gitignore so get from Don
   scopes: ['https://www.googleapis.com/auth/drive'],
 });
 
 const drive = google.drive({ version: 'v3', auth });
 
-// 2. Upload Image Function
 export async function uploadImage(fileBuffer: Buffer, originalName: string) {
-  const mimeType = mime.lookup(originalName) || 'application/octet-stream'; // Dynamically determine MIME type
+  const mimeType = mime.lookup(originalName) || 'application/octet-stream'; 
 
   const fileMetadata = {
-    name: originalName, // Use the original file name
-    parents: ['1WcX-1BNWnvm3RvZTxQQMdLtFSHDYp8Fw'], // ID of the folder you shared
+    name: originalName, 
+    parents: ['1WcX-1BNWnvm3RvZTxQQMdLtFSHDYp8Fw'], 
   };
 
   const media = {
-    mimeType: mimeType, // Use the dynamically determined MIME type
-    body: Readable.from(fileBuffer), // Create a readable stream from the buffer
+    mimeType: mimeType, 
+    body: Readable.from(fileBuffer),
   };
 
   try {
@@ -33,10 +31,10 @@ export async function uploadImage(fileBuffer: Buffer, originalName: string) {
     });
 
     console.log('Uploaded File ID:', response.data.id);
-    return response.data.id; // Return the file ID for further use
+    return response.data.id; 
   } catch (error) {
     console.error('Error uploading file to Google Drive:', error);
-    throw error; // Re-throw the error to handle it in the calling function
+    throw error;
   }
 }
 
